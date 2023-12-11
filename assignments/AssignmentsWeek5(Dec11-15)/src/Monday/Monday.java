d2a3a6542cf6c1ad9af10c20fdfe359801f3d0ab
Samuel Park <parksamuelsoung@gmail.com>
2023-12-11 13:32:08 -0600

    Week 5 Monday Assignment

---
+++ assignments/AssignmentsWeek5(Dec11-15)/src/Monday/Monday.java
@@ -0,0 +1,21 @@
+
+/*
+
+Samuel Park
+Week 5 - Monday
+
+1. 3 coding conventions I am good at following already are:
+    4.1 Line Length (keeping lines short so that they are visible), 
+    7.4 if, if-else, if else-if else statements (keeping form with {}), 
+    8.1 Blank Lines (between sections, methods, and class definitions)
+
+2. 3 coding conventions I will start using more consistently are:
+    8.2 Blank Spaces (a keyword followed by () should be separated with a space),
+    10.5.2 Returning Values (making intent of code clear and straight to the point),
+    6.2 Initialization (try to initialize local variables where declared)
+
+3. It is important to follow coding conventions and write clean code because
+    it allows code to be read, understandable, and updatable by other 
+    developers when working in a team. It also benefits the original developer, as
+    the original developer can also have better readability of their own code. 
+*/
