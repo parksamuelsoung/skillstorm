b85dbe59e5e7d184badf2adc06e3031d6156707a
Samuel Park <parksamuelsoung@gmail.com>
2023-12-15 13:57:07 -0600

    Week 4 Tuesday and Wednesday Assignment

---
+++ assignments/AssignmentsWeek4(Dec4-8)/Tuesday-Abstraction/src/com/skillstorm/Fillable.java
@@ -0,0 +1,13 @@
+package com.skillstorm;
+
+public interface Fillable {
+
+    public void fill();
+
+    public void empty();
+
+    public void fill(double quantity);
+
+    public void empty(double quantity);
+
+}
