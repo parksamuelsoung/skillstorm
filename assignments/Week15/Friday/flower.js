6f09da4d5cddab2c7c022349de4c653b04c186b0
Samuel Park <parksamuelsoung@gmail.com>
2024-03-01 23:38:30 -0600

    Week 15 Friday Assignment

--- assignments/Week15/Friday/flower.js
+++ assignments/Week15/Friday/flower.js
@@ -1,33 +1,22 @@
 
 
 // DOM
-let header = document.getElementById("header");
-let button = document.getElementById("buy-button");
-let image = document.getElementById("image");
-let description = document.getElementById("description");
-let fallButton = document.getElementById("fall-button");
+let page = document.getElementById("page");
+let header = page.querySelector("h1");
+let button = page.querySelector("p").querySelector("button");
+let image = page.querySelector("img");
+let description = page.querySelector("p");
 let fall = document.getElementById("fall");
 let fallImage = document.getElementById("fall-image");
-let fallImageLarge = document.getElementById("fall-image-large");
-let springButton = document.getElementById("spring-button");
 let spring = document.getElementById("spring");
 let springImage = document.getElementById("spring-image");
-let springImageLarge = document.getElementById("spring-image-large");
-let summerButton = document.getElementById("summer-button");
 let summer = document.getElementById("summer");
 let summerImage = document.getElementById("summer-image");
-let summerImageLarge = document.getElementById("summer-image-large");
-let winterButton = document.getElementById("winter-button");
 let winter = document.getElementById("winter");
 let winterImage = document.getElementById("winter-image");
-let winterImageLarge = document.getElementById("winter-image-large");
 
 // Event Listeners
 button.addEventListener("click", soldOut);
-fallButton.addEventListener("click", soldOut);
-springButton.addEventListener("click", soldOut);
-summerButton.addEventListener("click", soldOut);
-winterButton.addEventListener("click", soldOut);
 fallImage.addEventListener("click", fallPage);
 springImage.addEventListener("click", springPage);
 summerImage.addEventListener("click", summerPage);
@@ -39,22 +28,26 @@ function soldOut() {
 }
 
 function fallPage() {
-    image.src = fallImageLarge.src;
+    image.src = fall.querySelector("img").src;
     header.innerText = fall.querySelector("h1").innerText;
+    description.insertBefore(button.innerText = fall.querySelector("p").querySelector("button").innerText);
     description.innerText = fall.querySelector("p").innerText;
 }
 function springPage() {
-    image.src = springImageLarge.src;
+    image.src = spring.querySelector("img").src;
     header.innerText = spring.querySelector("h1").innerText;
+    description.insertBefore(button.innerText = spring.querySelector("p").querySelector("button").innerText);
     description.innerText = spring.querySelector("p").innerText;
 }
 function summerPage() {
-    image.src = summerImageLarge.src;
+    image.src = summer.querySelector("img").src;
     header.innerText = summer.querySelector("h1").innerText;
+    description.insertBefore(button.innerText = summer.querySelector("p").querySelector("button").innerText);
     description.innerText = summer.querySelector("p").innerText;
 }
 function winterPage() {
-    image.src = winterImageLarge.src;
+    image.src = winter.querySelector("img").src;
     header.innerText = winter.querySelector("h1").innerText;
+    description.insertBefore(button.innerText = winter.querySelector("p").querySelector("button").innerText);
     description.innerText = winter.querySelector("p").innerText;
 }
