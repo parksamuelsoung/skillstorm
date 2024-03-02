0864182053fe6bc5e549bb86cda4e920b6ec0dfc
Samuel Park <parksamuelsoung@gmail.com>
2024-03-02 00:08:52 -0600

    Week 15 Friday Assignment

--- assignments/Week15/Friday/flower.js
+++ assignments/Week15/Friday/flower.js
@@ -30,24 +30,24 @@ function soldOut() {
 function fallPage() {
     image.src = fall.querySelector("img").src;
     header.innerText = fall.querySelector("h1").innerText;
-    description.insertBefore(button.innerText = fall.querySelector("p").querySelector("button").innerText);
+    button.innerText = fall.querySelector("p").querySelector("button").innerText;
     description.innerText = fall.querySelector("p").innerText;
 }
 function springPage() {
     image.src = spring.querySelector("img").src;
     header.innerText = spring.querySelector("h1").innerText;
-    description.insertBefore(button.innerText = spring.querySelector("p").querySelector("button").innerText);
+    button.innerText = spring.querySelector("p").querySelector("button").innerText;
     description.innerText = spring.querySelector("p").innerText;
 }
 function summerPage() {
     image.src = summer.querySelector("img").src;
     header.innerText = summer.querySelector("h1").innerText;
-    description.insertBefore(button.innerText = summer.querySelector("p").querySelector("button").innerText);
+    button.innerText = summer.querySelector("p").querySelector("button").innerText;
     description.innerText = summer.querySelector("p").innerText;
 }
 function winterPage() {
     image.src = winter.querySelector("img").src;
     header.innerText = winter.querySelector("h1").innerText;
-    description.insertBefore(button.innerText = winter.querySelector("p").querySelector("button").innerText);
+    button.innerText = winter.querySelector("p").querySelector("button").innerText;
     description.innerText = winter.querySelector("p").innerText;
 }
